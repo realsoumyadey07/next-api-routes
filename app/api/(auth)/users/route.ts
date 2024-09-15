51ea0db853dde242093485cff7cae18d97cdefd6
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-09-15 20:36:53 +0530

    CRUD apis are being designed

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,13 +1,94 @@
-import connectDb from "@/lib/db"
+import connectDb from "@/lib/db";
 import User from "@/lib/models/user.model";
-import { NextResponse } from "next/server"
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
 
-export const GET = async()=> {
+const ObjectId = require("mongoose").Types.ObjectId;
+
+export const GET = async () => {
+  try {
+    await connectDb();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Error while fetching all users" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connectDb();
+    const newUser = new User(body);
+    await newUser.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is created", user: newUser }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log("Error has occurred while creating the user", error.message);
+
+    return new NextResponse(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+};
+
+export const PATCH = async(request: Request)=> {
     try {
+        const body = await request.json();
+        const { userId, newUserName } = body;
         await connectDb();
-        const users = await User.find();
-        return new NextResponse(JSON.stringify(users), {status: 200});
+        if(!userId || !newUserName){
+            return new NextResponse(JSON.stringify({message: "ID or new username is required!"}),
+            {status: 400}
+            );
+        }
+        if(!Types.ObjectId.isValid(userId)){
+            return new NextResponse(JSON.stringify({message: "Invalid user id"}),
+            {status: 400}
+            )
+        }
+        const updatedUser = await User.findOneAndUpdate(
+            {_id: new ObjectId(userId)},
+            {username: newUserName},
+            {new: true}
+        );
+        if(!updatedUser){
+            return new NextResponse(JSON.stringify({message: "User not found in the database!"}),
+            {status: 400}
+            )
+        }
+        return new NextResponse(JSON.stringify({message: "user is updated successfully", user: updatedUser}), {
+            status: 200
+        })
     } catch (error: any) {
-        return new NextResponse("Error while fetching all users"+ error.message, {status: 500});
+        return new NextResponse(JSON.stringify({message: error.message}),
+        {status: 500}
+        )
+    }
+}
+
+export const DELETE = async(request: Request)=> {
+    try {
+        const {searchParams} = new URL(request.url);
+        const userId = searchParams.get("userId");
+
+        if(!userId) {
+            return new NextResponse(JSON.stringify({message: "ID or new username not found!"}),
+            {status: 400}
+            )
+        }
+        if(!Types.ObjectId.isValid(userId)){
+            return new NextResponse(JSON.stringify({message: "Invalid user id"}),
+            { status: 400}
+            )
+        }
+
+    } catch (error: any) {
+        
     }
 }
